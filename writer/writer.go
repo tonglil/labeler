fd8f2315a8811af1c922c75926a1f4f8423cd41d
Tony Li <tonyli.lives@gmail.com>
2016-12-02 02:49:26 -0500

    add logic spec

--- writer/writer.go
+++ writer/writer.go
@@ -6,6 +6,37 @@ import (
 	"github.com/google/go-github/github"
 )
 
-func Run(client *github.Client, file string, opt *types.Options) {
+var steps = `
+1. Rename: intersection of local (l.From) + remote (l.Name)
+2. Update: intersection of local (l.Name) + remote (l.Name)
+3. Create: l.Name compliment of remote
+4. Delete: l.Name compliment of locale
+`
+
+func Run(client *github.Client, file string, opt *types.Options) error {
+	// open file
+	// unmarshal file contents
+	// configure the right repo
+	// ep = [https://api.github.com/] repos/ [tonglil/labeler] /labels
+	// get all remote labels from repo
+	// for l in local labels with "from:"
+	// if l doesn't exist
+	//   if l.From exists
+	//     rename from to it
+	//       PATCH ep + /:name <- l.from
+	//         {
+	//           "name": "l.name",
+	//           "color": "l.color"
+	//         }
+	//   else create it
+	//     POST ep
+	//       {
+	//         "name": "l.name",
+	//         "color": "l.color"
+	//       }
+	// do create non-existing local labels
+	// do update existing local labels
+	// do delete remaining remote labels
+
 	return
 }
