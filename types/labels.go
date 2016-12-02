bc625e33734713073e99408ff29d712267f616a4
Tony Li <tonyli.lives@gmail.com>
2016-12-02 02:09:46 -0500

    add the label types

---
+++ types/labels.go
@@ -0,0 +1,12 @@
+package types
+
+type File struct {
+	Repo   string  `yaml:"repo"`
+	Labels []Label `yaml:"labels"`
+}
+
+type Label struct {
+	Name  string `yaml:"name"`
+	Color string `yaml:"color"`
+	From  string `yaml:"from,omitempty"`
+}
