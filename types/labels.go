90094b659270721f0a25d516997283c0e050c00c
Tony Li <tonyli.lives@gmail.com>
2016-12-06 00:39:43 -0500

    add docs to types.labels

--- types/labels.go
+++ types/labels.go
@@ -1,10 +1,12 @@
 package types
 
+// LabelFile represents a labels.yaml file.
 type LabelFile struct {
 	Repo   string  `yaml:"repo"`
 	Labels []Label `yaml:"labels"`
 }
 
+// Label represents a label in the labels.yaml file.
 type Label struct {
 	Name  string `yaml:"name"`
 	Color string `yaml:"color"`
