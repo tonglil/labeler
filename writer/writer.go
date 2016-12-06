279bbf38c55109b938b8c35edd23f498921aad84
Tony Li <tonyli.lives@gmail.com>
2016-12-06 02:05:30 -0500

    remove steps/instructions

--- writer/writer.go
+++ writer/writer.go
@@ -13,34 +13,8 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-var steps = `
-1. Rename: intersection of local (l.From) + remote (l.Name)
-2. Update: intersection of local (l.Name) + remote (l.Name)
-3. Create: l.Name compliment of remote
-4. Delete: l.Name compliment of locale
-`
-
 // Run executes the write actions against the repo.
 func Run(client *github.Client, file string, opt *types.Options) error {
-	// for l in local labels with "from:"
-	// if l doesn't exist
-	//   if l.From exists
-	//     rename from to it
-	//       PATCH ep + /:name <- l.from
-	//         {
-	//           "name": "l.name",
-	//           "color": "l.color"
-	//         }
-	//   else create it
-	//     POST ep
-	//       {
-	//         "name": "l.name",
-	//         "color": "l.color"
-	//       }
-	// do create non-existing local labels
-	// do update existing local labels
-	// do delete remaining remote labels
-
 	lf, err := ReadConfigFile(file)
 	if err != nil {
 		return err
