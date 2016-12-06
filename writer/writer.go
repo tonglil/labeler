a7b20bf8c7e0b7633279f27b473e11f4ff01ed11
Tony Li <tonyli.lives@gmail.com>
2016-12-05 23:50:55 -0500

    read file and get repo name

--- writer/writer.go
+++ writer/writer.go
@@ -1,8 +1,16 @@
 package writer
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	yaml "gopkg.in/yaml.v2"
+
 	"github.com/tonglil/labeler/types"
 
+	"github.com/golang/glog"
 	"github.com/google/go-github/github"
 )
 
@@ -37,6 +45,54 @@ func Run(client *github.Client, file string, opt *types.Options) error {
 	// do create non-existing local labels
 	// do update existing local labels
 	// do delete remaining remote labels
+	lf, err := ReadConfigFile(file)
+	if err != nil {
+		return err
+	}
 
-	return
+	if opt.Repo == "" {
+		opt.Repo = lf.Repo
+	}
+
+	if opt.Repo == "" {
+		glog.V(0).Infof("No repo provided")
+		return fmt.Errorf("no repo")
+	}
+
+	return nil
+}
+
+func ReadConfigFile(file string) (*types.LabelFile, error) {
+	path, err := filepath.Abs(file)
+	if err != nil {
+		glog.V(0).Infof("Failed to find %s", file)
+		return nil, err
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		glog.V(0).Infof("Failed to open %s", path)
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		glog.V(0).Infof("Failed to read %s", path)
+		return nil, err
+	}
+
+	glog.V(4).Infof("Read file %s", path)
+
+	lf := types.LabelFile{}
+
+	err = yaml.Unmarshal(data, &lf)
+	if err != nil {
+		glog.V(0).Infof("Failed to unmarshal %s", path)
+		return nil, err
+	}
+
+	glog.Info(lf)
+
+	return &lf, nil
 }
